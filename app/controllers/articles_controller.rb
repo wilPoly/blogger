4ab9367f14fdbf8447a769f6c21ea08603bd2293
wilPoly <misterwrinkle@gmail.com>
2017-10-03 18:37:50 +0200

    Add Edit action and flash notice upon creation, update and deletion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,9 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		@article.save
+
+		flash.notice = "Article #{@article.title} created!"
+
 		redirect_to article_path(@article)
 	end
 
@@ -18,9 +21,25 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article #{@article.title} updated!"
+
+		redirect_to article_path(@article)
+	end
+
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
+
+		flash.notice = "Article #{@article.title} deleted!"
+
 		redirect_to articles_path(@articles)
 	end
 
